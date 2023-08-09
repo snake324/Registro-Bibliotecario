4b6e2d787b52ca66cd7ea06812b99c6ce0a1e4d5
snake324 <kristi4n90@gmail.com>
2023-08-09 12:19:20 +0200

    service created

---
+++ src/app/service/book.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private apiUrl = 'http://localhost:3000/books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.apiUrl, book);
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    const url = `${this.apiUrl}/${book.id}`;
+    return this.http.put<Book>(url, book);
+  }
+
+  deleteBook(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+}
